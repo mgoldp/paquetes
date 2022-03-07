6a008b915571f57723eec0de179c696a193ae6d7
mgoldp <mgoldp2013@gmail.com>
2022-03-07 18:19:10 -0300

    Mapear la respuesta a una instancia de clase Parte 2

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,24 @@
-import 'dart:convert';
+//import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+
+import 'package:paquetes/clases/reqres_respuesta.dart';
 
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
   final url = Uri.parse('https://reqres.in/api/users?page=2#');
   http.get(url).then((res) {
+    final resReqRes = reqRespRespuestaFromJson(res.body);
     //print(res);
-    final body = jsonDecode(res.body);
-    print(body);
-    print('page: ${body['page']}');
-    print('per_page: ${body['per_page']} ');
-    print('id del tercer elemento: ${body['data'][2]['id']} ');
+    //final body = jsonDecode(res.body);
+    //print(body);
+    //print('page: ${body['page']}');
+    //print('per_page: ${body['per_page']} ');
+    //print('id del tercer elemento: ${body['data'][2]['id']} ');
+
+    print('page: ${resReqRes.page}');
+    print('per_page: ${resReqRes.perPage} ');
+    print('id del tercer elemento: ${resReqRes.data[2].id}');
   });
 }
--- lib/clases/reqres_respuesta.dart
+++ lib/clases/reqres_respuesta.dart
@@ -12,21 +12,89 @@ String reqRespRespuestaToJson(ReqRespRespuesta data) =>
 
 class ReqRespRespuesta {
   ReqRespRespuesta({
-    this.greeting,
-    this.instructions,
+    required this.page,
+    required this.perPage,
+    required this.total,
+    required this.totalPages,
+    required this.data,
+    required this.support,
   });
 
-  String? greeting;
-  List<String>? instructions;
+  int page;
+  int perPage;
+  int total;
+  int totalPages;
+  List<Persona> data;
+  Support support;
 
   factory ReqRespRespuesta.fromJson(Map<String, dynamic> json) =>
       ReqRespRespuesta(
-        greeting: json["greeting"],
-        instructions: List<String>.from(json["instructions"].map((x) => x)),
+        page: json["page"],
+        perPage: json["per_page"],
+        total: json["total"],
+        totalPages: json["total_pages"],
+        data: List<Persona>.from(json["data"].map((x) => Persona.fromJson(x))),
+        support: Support.fromJson(json["support"]),
       );
 
   Map<String, dynamic> toJson() => {
-        "greeting": greeting,
-        "instructions": List<dynamic>.from(instructions!.map((x) => x)),
+        "page": page,
+        "per_page": perPage,
+        "total": total,
+        "total_pages": totalPages,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "support": support.toJson(),
+      };
+}
+
+class Persona {
+  Persona({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  factory Persona.fromJson(Map<String, dynamic> json) => Persona(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
+}
+
+class Support {
+  Support({
+    this.url,
+    this.text,
+  });
+
+  String? url;
+  String? text;
+
+  factory Support.fromJson(Map<String, dynamic> json) => Support(
+        url: json["url"],
+        text: json["text"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "url": url,
+        "text": text,
       };
 }
