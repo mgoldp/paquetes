c8f46a0ae04b789eec7a6a31bbeb7dc4f8ac35a6
mgoldp <mgoldp2013@gmail.com>
2022-03-07 17:15:58 -0300

    extraer y usar el body de una petición HTTP

--- bin/main.dart
+++ bin/main.dart
@@ -7,6 +7,11 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 void main(List<String> arguments) {
   final url = Uri.parse('https://reqres.in/api/users?page=2#');
   http.get(url).then((res) {
-    print(res);
+    //print(res);
+    final body = jsonDecode(res.body);
+    print(body);
+    print('page: ${body['page']}');
+    print('per_page: ${body['per_page']} ');
+    print('id del tercer elemento: ${body['data'][2]['id']} ');
   });
 }
